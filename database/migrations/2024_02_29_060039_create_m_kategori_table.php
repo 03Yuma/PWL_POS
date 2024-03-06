4d1cc7284b3d1230de821798e0e7551bfe50d11c
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-03-06 20:19:07 +0700

    Migration

--- database/migrations/2024_02_29_060039_create_m_kategori_table.php
+++ database/migrations/2024_02_29_060039_create_m_kategori_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -12,7 +13,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
+            $table->id('kategori_id');
+            $table->string('kategori_kode', 10)->unique();
+            $table->string('kategori_nama', 100);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2024_03_06_123325_create_t_penjualan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->unsignedBigInteger('barang_id');
+            $table->integer('harga');
+            $table->integer('jumlah');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('m_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('t_penjualan', function (Blueprint $table) {
+            //
+        });
+    }
+};
