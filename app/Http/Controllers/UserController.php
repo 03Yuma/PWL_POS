1b0d7b94e4341daabc9d2b99f024b52b8e763452
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-03-13 21:33:25 +0700

    praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,8 +33,20 @@ class UserController extends Controller
         // ];
         // UserModel::create($data);
 
-        $user=UserModel::where('level_id',2)->count();
-        // dd($user);
+        // $user=UserModel::where('level_id',2)->count();
+        // // dd($user);
+        // return view('user',['data'=>$user]);
+
+        $user = UserModel::firstOrCreate(
+            [
+                'username' =>'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password'=>Hash::make('12345'),
+                'level_id'=>2
+            ],
+        );
+        $user->save();
+        
         return view('user',['data'=>$user]);
     }
 }
